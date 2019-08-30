207d42366bffb6c9303238d8faab638acfdf7fa7
unknown <param.holla@aot-technologies.com>
2019-08-30 15:21:36 -0700

    Adding Final Code

--- workflow/src/main/java/com/aot/pharma/workflow/ConfigurationProperties.java
+++ workflow/src/main/java/com/aot/pharma/workflow/ConfigurationProperties.java
@@ -1,30 +1,23 @@
 package com.aot.pharma.workflow;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
 public class ConfigurationProperties {
 
-	private static final String CONFIG_FILE = "/pharma.properties";
+	private static final String CONFIG_FILE = "pharma.properties";
 
-	public static File readFileFromClasspath() {
-		final URL fileUrl = ConfigurationProperties.class.getResource(CONFIG_FILE);
-		return new File(fileUrl.getFile());
-	}
 
 	public static Properties getConfiguration() throws FileNotFoundException, IOException {
-		final Properties properties = new Properties();
-		try {
-			InputStream input = new FileInputStream(readFileFromClasspath());
-			properties.load(input);
-		}catch (Exception e) {
-			
-		}
+		final Properties properties;
+        try (InputStream input = ConfigurationProperties.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+        	properties = new Properties();
+            if (input != null) {
+            	properties.load(input);
+            }
+        }
 		return properties;
 	}
 }
