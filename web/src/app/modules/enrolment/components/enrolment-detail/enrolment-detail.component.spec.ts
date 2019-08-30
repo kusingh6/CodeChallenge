c9502fb8dac86132a0a76f30a3ac2e6a8db95ca4
Vysakh Menon <vysakh.menon@aot-technologies.com>
2019-08-30 14:32:54 -0700

    bug fix and test cases

--- web/src/app/modules/enrolment/components/enrolment-detail/enrolment-detail.component.spec.ts
+++ web/src/app/modules/enrolment/components/enrolment-detail/enrolment-detail.component.spec.ts
@@ -1,32 +1,65 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EnrolmentDetailComponent } from './enrolment-detail.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { KeycloakService } from 'keycloak-angular';
 import { EnrolmentService } from '../../services/enrolment.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('EnrolmentDetailComponent', () => {
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule,
-          HttpClientTestingModule,
-          ReactiveFormsModule
-        ],
-        declarations: [
-          EnrolmentDetailComponent
-        ],
-        providers: [
-          KeycloakService,
-          EnrolmentService
-        ]
-      }).compileComponents();
-    }));
+class MockKeycloakService {
+  getKeycloakInstance() {
+    return {
+      profile: {
+        lastName: '',
+        firstName: '',
+        username: 'vysakh'
+      }
+    };
+  }
+}
 
-    it('should create the enrolment-detail', () => {
-      const fixture = TestBed.createComponent(EnrolmentDetailComponent);
-      const enrolmentDetail = fixture.debugElement.componentInstance;
-      expect(enrolmentDetail).toBeTruthy();
-    });
+describe('EnrolmentDetailComponent', () => {
+  let component: EnrolmentDetailComponent;
+  let fixture: ComponentFixture<EnrolmentDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        FormsModule
+      ],
+      declarations: [
+        EnrolmentDetailComponent
+      ],
+      providers: [
+        { provide: KeycloakService, useClass: MockKeycloakService },
+        EnrolmentService
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EnrolmentDetailComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+    fixture.detectChanges();
+  });
+
+  it('should create the enrolment-detail', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('enrolmentForm invalid when empty', () => {
+    expect(component.enrolmentForm.valid).toBeTruthy();
+  });
+
+  it('enrolmentForm submit', () => {
+    // tslint:disable-next-line: no-string-literal
+    component.enrolmentForm.controls['pharmacistRegNo'].setValue('1234');
+    // tslint:disable-next-line: no-string-literal
+    component.enrolmentForm.controls['fullName'].setValue('vysakh');
+});
+
 });
--- web/src/app/modules/enrolment/components/enrolment-status/enrolment-status.component.spec.ts
+++ web/src/app/modules/enrolment/components/enrolment-status/enrolment-status.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EnrolmentStatusComponent } from './enrolment-status.component';
 import { EnrolmentResponse } from '../../models/enrolment-response';
@@ -17,22 +17,34 @@ class RouterStub {
   }
 }
 
-describe('EnrolmentDetailComponent', () => {
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule
-        ],
-        declarations: [
-          EnrolmentStatusComponent
-        ],
-        providers: [{ provide: Router, useClass: RouterStub }]
-      }).compileComponents();
-    }));
+describe('EnrolmentStatusComponent', () => {
+  let component: EnrolmentStatusComponent;
+  let fixture: ComponentFixture<EnrolmentStatusComponent>;
 
-    it('should create the enrolment-status', () => {
-      const fixture = TestBed.createComponent(EnrolmentStatusComponent);
-      const enrolmentStatus = fixture.debugElement.componentInstance;
-      expect(enrolmentStatus).toBeTruthy();
-    });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        EnrolmentStatusComponent
+      ],
+      providers: [{ provide: Router, useClass: RouterStub }]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EnrolmentStatusComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+    fixture.detectChanges();
+  });
+
+  it('should create the enrolment-status', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('enrolmentResponse has valid referenceId', () => {
+    expect(component.enrolmentResponse.referenceId).toBe('1234');
+  });
 });
